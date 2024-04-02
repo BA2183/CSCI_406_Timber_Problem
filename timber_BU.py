fb22e1b639f7ed26c2fddcef6928078b9a09b947
dcnguyen <dcnguyen@mines.edu>
2024-04-02 13:45:58 -0600

    set up to test

--- timber_BU.py
+++ timber_BU.py
@@ -1,3 +1,8 @@
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+plt.style.use("Solarize_Light2")
+
 def read_input():
     arr_length = int(input())
     in_arr = [item for item in map(int, input().split())]
@@ -25,5 +30,32 @@ def timber(in_arr, head, tail):
         # return result from desired input
     return mem[head][tail]
 
-length, arr = read_input()
-print(timber(arr, 0, length-1))
+def perform_test(n):
+    iter_arr = []
+    time_arr = []
+    for i in range(1, n+1):
+        # random timber generators
+        curr_arr = np.random.randint(1, 1001, size=n)
+        # timer
+        start_time = time.time()
+        timber(curr_arr, 0, i-1)
+        end_time = time.time()
+        time_diff = end_time - start_time
+        # record
+        time_arr.append(time_diff)
+        iter_arr.append(i)
+
+        print(f"done iter: {i}")
+
+    return time_arr, iter_arr
+# length, arr = read_input()
+# print(timber(arr, 0, length-1))
+
+time_arr, iter_arr = perform_test(2000)
+# plot
+plt.plot(iter_arr, time_arr)
+plt.xticks(iter_arr)
+plt.xlabel("timber length")
+plt.ylabel("time (seconds)")
+plt.title("Timber() performance based on timber length")
+plt.show()
