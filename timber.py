b148efb268c252d52fb468396589b8af401e7cdb
dcnguyen <dcnguyen@mines.edu>
2024-04-01 19:59:19 -0600

    added plot

--- timber.py
+++ timber.py
@@ -15,25 +15,24 @@ def perform_test(n):
     iter_arr = []
     time_arr = []
     for i in range(1, n+1):
+        # random timber generators
         curr_arr = np.random.randint(1, 1001, size=n)
-
+        # timer
         start_time = time.time()
         timber(curr_arr, 0, i-1)
         end_time = time.time()
-
         time_diff = end_time - start_time
-
+        # record
         time_arr.append(time_diff)
         iter_arr.append(i)
 
     return time_arr, iter_arr
 
-# length, timbers = read_input()
-# print(f"input: {(length, timbers)}")
-# timbers = np.array([5, 6, 9, 7])
-# length = 4
-# out = timber(timbers, 0, length-1)
-# print(out)
 time_arr, iter_arr = perform_test(20)
+# plot
 plt.plot(iter_arr, time_arr)
+plt.xticks(iter_arr)
+plt.xlabel("timber length")
+plt.ylabel("time (seconds)")
+plt.title("Timber() performance based on timber length")
 plt.show()
