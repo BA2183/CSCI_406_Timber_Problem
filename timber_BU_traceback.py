8a9e582a4769f04bbd353e91fe832b8c161d31ab
dcnguyen <dcnguyen@mines.edu>
2024-04-30 08:10:42 -0600

    fixed timing project

--- timber_BU_traceback.py
+++ timber_BU_traceback.py
@@ -3,23 +3,33 @@ def read_input():
     in_arr = [item for item in map(int, input().split())]
     return arr_length, in_arr
 
-def timber(in_arr, head, tail):
+# calculate the segment sum to reduce sum calculation
+def segment_sum(in_arr, length):
+    # empty table
+    sum_arr = [[0 for _ in range(length+1)] for _ in range(length+1)]
+    for i in range(length):
+        sum_arr[i][i] = in_arr[i]
+        for j in range(i+1, length):
+            sum_arr[i][j] = sum_arr[i][j - 1] + in_arr[j]
+
+    return sum_arr
+def timber(in_arr, sum_arr, length):
     # init memory
-    mem = [[(in_arr[row], 'S') if row == col else (0, ' ') for row in range(tail+1)] for col in range(tail+1)]
+    mem = [[(in_arr[row], 'S') if row == col else (0, ' ') for row in range(length)] for col in range(length)]
     # starting j
     start_j = 1
-    while start_j < tail+1:
+    while start_j < length:
         # this work that i reset to 0 every time, go back to the top row
         # while j only restart where j where originally with one step a head
         j = start_j
         i = 0
-        while i < tail+1 and j < tail+1:
+        while i < length and j < length:
             # relation
             if i != j and i < j:
                 if mem[i+1][j][0] <= mem[i][j-1][0]:
-                    mem[i][j] = (sum(in_arr[i:j+1]) - mem[i+1][j][0], 'L')
+                    mem[i][j] = (sum_arr[i][j] - mem[i+1][j][0], 'L')
                 elif mem[i][j-1][0] < mem[i+1][j][0]:
-                    mem[i][j] = (sum(in_arr[i:j+1]) - mem[i][j-1][0], 'R')
+                    mem[i][j] = (sum_arr[i][j] - mem[i][j-1][0], 'R')
             # move both j and i
             j += 1
             i += 1
@@ -51,7 +61,8 @@ def process_traceback(length, p):
     return ' '.join(result)
     
 length, arr = read_input()
-output_matrix = timber(arr, 0, length-1)
+sum_table = segment_sum(arr, length)
+output_matrix = timber(arr, sum_table, length)
 result = output_matrix[0][length-1][0]
 traceback = process_traceback(length, output_matrix)
 print(result)
